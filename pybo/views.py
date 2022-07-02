f1e46825ec9eb2cb09abdc83ffa1446c6741b7c1
kimdongwoo3350@gmail.com <kimdongwoo3350@gmail.com>
2022-07-02 16:46:31 +0900

    4:46

--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,12 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
+from pybo.models import Question
 # Create your views here.
 
 def index(request):
-    return HttpResponse("hello django")
+    #질문목록 출력
+    question_list=Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request,'pybo/question_list.html', context)
+    
